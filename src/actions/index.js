60fd855f8ea429d8b509e84c3d47300b5abe7509
kenshinji <gundam0083ster@gmail.com>
2017-07-31 22:50:38 +0800

    finish lecture 58

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const API_KEY = 'd923c7d661adee7c8d9d814b166bf8af';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER'
-export function(city){
+export function fetchWeather(city){
   const url = `${ROOT_URL}&q=${city},us`;
   const request = axios.get(url);
 
